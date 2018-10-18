3e8d9e3571dbf75b7134fa8712a5c3404853377f
h_miyamoto <h_miyamoto@r-jc.jp>
2018-10-18 17:55:53 +0900

    developブランチ作成

--- app/src/main/java/rjc/co/jp/myphotoretouch/activity/MainActivity.kt
+++ app/src/main/java/rjc/co/jp/myphotoretouch/activity/MainActivity.kt
@@ -1,6 +1,20 @@
 package rjc.co.jp.myphotoretouch.activity
 
+import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
 
-class MainActivity : AppCompatActivity(){}
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+
+    override fun onResume() {
+        super.onResume()
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+    }
+}
 
