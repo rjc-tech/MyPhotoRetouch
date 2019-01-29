9ba23d3f1b11b25b4637976fbbac088c96dbb1cd
yoshidamariko <m_yoshida@r-jc.jp>
2019-01-29 18:46:38 +0900

    保存ボタン押下時ダイアログ作成（ボタン）

--- app/src/main/java/rjc/co/jp/myphotoretouch/activity/MainActivity.kt
+++ app/src/main/java/rjc/co/jp/myphotoretouch/activity/MainActivity.kt
@@ -4,14 +4,16 @@ import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
 import android.view.Menu
 import android.view.MenuItem
-import android.view.View
 import rjc.co.jp.myphotoretouch.R
+import android.content.DialogInterface;
+
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
     }
 
     override fun onResume() {
@@ -30,7 +32,25 @@ class MainActivity : AppCompatActivity() {
 
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
         when (item.itemId) {
-            R.id.save_image ->{}
+            R.id.save_image ->{
+                // どの選択肢が選ばれたかを保持する変数
+                var selectedId = 0
+                // ダイアログを表示する
+                var dialog = ConfirmDialog()
+                dialog.title = "保存ファイル名"
+                dialog.msg = "参照ファイル"
+                dialog.onRadioClickListener = DialogInterface.OnClickListener{dialog, id ->
+                    selectedId = id
+                }
+                dialog.okText = "OK"
+                dialog.cancelText = "キャンセル"
+                dialog.onOkClickListener = DialogInterface.OnClickListener { dialog, id ->
+                }
+                dialog.onCancelClickListener = DialogInterface.OnClickListener { dialog, id ->
+                }
+                // supportFragmentManagerはAppCompatActivity(正確にはFragmentActivity)を継承したアクティビティで使用可
+                dialog.show(supportFragmentManager, "tag")
+            }
             R.id.load_image ->{}
             R.id.about ->{}
         }
