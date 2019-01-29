78f5f63eb5c1da290e6df32704dad12bd3b1fba3
h_miyamoto <h_miyamoto@r-jc.jp>
2019-01-29 11:55:13 +0900

    レイアウト実装

--- app/src/main/java/rjc/co/jp/myphotoretouch/activity/MainActivity.kt
+++ app/src/main/java/rjc/co/jp/myphotoretouch/activity/MainActivity.kt
@@ -2,11 +2,16 @@ package rjc.co.jp.myphotoretouch.activity
 
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import android.view.Menu
+import android.view.MenuItem
+import android.view.View
+import rjc.co.jp.myphotoretouch.R
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
     }
 
     override fun onResume() {
@@ -15,6 +20,21 @@ class MainActivity : AppCompatActivity() {
 
     override fun onDestroy() {
         super.onDestroy()
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        menuInflater.inflate(R.menu.option, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when (item.itemId) {
+            R.id.save_image ->{}
+            R.id.load_image ->{}
+            R.id.about ->{}
+        }
+        return false
     }
 }
 
